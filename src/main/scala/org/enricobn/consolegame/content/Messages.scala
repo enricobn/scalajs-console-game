c059a648cfb269888ef5e5e5d8b3969192be7ce0
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-01-07 13:11:05 +0100

    Refactoring.

--- src/main/scala/org/enricobn/consolegame/content/Messages.scala
+++ src/main/scala/org/enricobn/consolegame/content/Messages.scala
@@ -10,23 +10,27 @@ import scala.collection.mutable.ListBuffer
   * Created by enrico on 12/25/16.
   */
 
-case class SerializableMessages(messages: List[String])
+case class SerializableMessages(messages: List[String]) {
+  def deserialize() = new Messages(Some(this))
+}
 
 object MessagesSerializer extends ContentSerializer[Messages] {
   override def toString(content: Messages): Either[IOError, String] = {
-    writeE(new SerializableMessages(content.messages))
+    writeE(content.serialize())
   }
 
   override def fromString(s: String): Either[IOError, Messages] = {
-    readE[SerializableMessages](s).right.map(m => new Messages(m.messages))
+    readE[SerializableMessages](s).right.map(_.deserialize())
   }
 
   override val clazz: Class[Messages] = classOf[Messages]
 }
 
-class Messages(initialMessages: Seq[String] = Seq()) {
-  private val _messages = new ListBuffer[String]() ++ initialMessages
+class Messages(serialized: Option[SerializableMessages] = None) {
+  private val _messages = new ListBuffer[String]()
   private val publisher = new StringPub()
+
+  serialized.foreach(_messages ++= _.messages)
 
   def messages = _messages.toList
 
@@ -44,4 +48,6 @@ class Messages(initialMessages: Seq[String] = Seq()) {
   }
 
   override def toString: String = _messages.mkString("\n")
+
+  def serialize() : SerializableMessages = SerializableMessages(_messages.toList)
 }
