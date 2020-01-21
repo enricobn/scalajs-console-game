51229be799b712f1fb4c20299f7773bca26e7f09
Enrico Benedetti <enrico_benedetti@yahoo.com>
2020-01-21 19:21:10 +0100

    Fixed SerializedFSOperationsSpec

--- src/test/scala/org/enricobn/consolegame/SerializedFSOperationsSpec.scala
+++ src/test/scala/org/enricobn/consolegame/SerializedFSOperationsSpec.scala
@@ -14,13 +14,16 @@ class SerializedFSOperationsSpec extends SpecWithShell {
         (serializer.clazz.getName, serializer)
       ).toMap
 
-  ignore should "deserialize" in {
+  "game" should "be deserializable" in {
     val f = fixture
-    val content = "{\"userName\":\"enrico\",\"fs\":{\"folders\":[{\"path\":\"/etc\",\"owner\":\"root\",\"permissions\":775},{\"path\":\"/bin\",\"owner\":\"root\",\"permissions\":775},{\"path\":\"/home\",\"owner\":\"root\",\"permissions\":775},{\"path\":\"/usr\",\"owner\":\"root\",\"permissions\":775},{\"path\":\"/var\",\"owner\":\"root\",\"permissions\":775},{\"path\":\"/home/enrico\",\"owner\":\"enrico\",\"permissions\":775},{\"path\":\"/home/enrico/Pisa\",\"owner\":\"enrico\",\"permissions\":775},{\"path\":\"/usr/bin\",\"owner\":\"root\",\"permissions\":775},{\"path\":\"/var/log\",\"owner\":\"root\",\"permissions\":775}],\"files\":[{\"path\":\"/home/enrico/Pisa/warehouse\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.buyandsell.content.Warehouse\",\"ser\":\"{\\\"goods\\\":{\\\"gold\\\":2,\\\"silver\\\":8,\\\"bronze\\\":20}}\"},{\"path\":\"/home/enrico/.history\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.shell.impl.StringList\",\"ser\":\"{\\\"value\\\":[\\\"ls\\\",\\\"sell Pisa/warehouse silver 2\\\"]}\"},{\"path\":\"/var/log/messages.log\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.consolegame.content.Messages\",\"ser\":\"{\\\"messages\\\":[\\\"sell 2 of silver\\\"]}\"},{\"path\":\"/etc/profile\",\"owner\":\"root\",\"permissions\":664,\"serializerName\":\"org.enricobn.shell.impl.StringMap\",\"ser\":\"{\\\"value\\\":{\\\"PATH\\\":\\\"/bin:/usr/bin\\\"}}\"},{\"path\":\"/home/enrico/gamestats\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.buyandsell.content.GameStatistics\",\"ser\":\"{\\\"money\\\":10000}\"},{\"path\":\"/home/enrico/Pisa/city\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.buyandsell.content.City\",\"ser\":\"{\\\"name\\\":\\\"Pisa\\\",\\\"statistics\\\":{\\\"population\\\":100,\\\"employed\\\":0}}\"},{\"path\":\"/home/enrico/.profile\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.shell.impl.StringMap\",\"ser\":\"{}\"},{\"path\":\"/home/enrico/market\",\"owner\":\"enrico\",\"permissions\":664,\"serializerName\":\"org.enricobn.buyandsell.content.Market\",\"ser\":\"{\\\"prices\\\":{\\\"gold\\\":1000,\\\"silver\\\":500,\\\"bronze\\\":100}}\"}]}}"
+    val content = rsc("buyandsell.json")
 
     (for {
+      _ <- f.shell.vum.addUser("user1", "user1")(f.rootAuthentication).toLeft(())
+      _ <- f.shell.vum.addUser("user2", "user2")(f.rootAuthentication).toLeft(())
       serializedGame <- UpickleUtils.readE[SerializedGame](content)
       _ <- SerializedFSOperations.load(f.shell, serializers, serializedGame.fs)(f.rootAuthentication)
+      _ <- f.shell.login("enrico", "enrico")
       home <- f.shell.homeFolder
       gamestats <- home.findFile("gamestats")(f.rootAuthentication)
     } yield {
@@ -28,6 +31,22 @@ class SerializedFSOperationsSpec extends SpecWithShell {
       assert(gamestats.isDefined)
     }).left.foreach{ e => fail(e.message) }
   }
+
+  /**
+    * from https://stackoverflow.com/questions/40866662/unit-testing-scala-js-read-test-data-from-file-residing-in-test-resources
+    */
+  def rsc(path: String): String = {
+    import scalajs.js.Dynamic.{global => g}
+    val fs = g.require("fs")
+
+    def readFile(name: String): String = {
+      fs.readFileSync(name).toString
+    }
+
+    readFile(rscPath(path))
+  }
+
+  def rscPath(path: String): String = "src/test/resources/" + path
 
 }
 
